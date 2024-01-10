c337be3a43b9c3a2cb36d463d1030d0974ae0b6c
João Vitor Dellarmelina <125659911+joaodellarmelina@users.noreply.github.com>
2024-01-10 11:34:27 -0300

    first commit

---
+++ Controllers/PaginaInicialController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetWeb.Controllers
+{
+    public class PaginaInicialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
